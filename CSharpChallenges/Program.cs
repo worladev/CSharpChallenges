f65a0963ee9787fbd5ed8e5e0ee9152ec296f046
worladev <confidenceworlasi@gmail.com>
2024-09-20 22:31:55 -0400

    solved task to print multiplication table using TryParse, while, and for loop

--- CSharpChallenges/Program.cs
+++ CSharpChallenges/Program.cs
@@ -10,7 +10,45 @@ namespace CSharpChallenges
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This is the init");
+            Console.WriteLine("This is the Main Challenges Solution Project.");
+
+            /* TASK
+             * Ask the user for a number x. Write a program to print x times table up to the 12th value.
+             */
+
+            // a default boolean variable to keep track of a valid number input.
+            bool isValid = true;
+
+            // a while loop to continue asking for number until a valid number is entered.
+            while (isValid)
+            {
+                Console.WriteLine("Enter a number to print it's time table.");
+                string userInput = Console.ReadLine();
+
+                // an if statement to check if the number entered is valid using the TryParse function
+                if (int.TryParse(userInput, out int value))
+                {
+                    // a for loop to print the times table
+                    for (int i = 1; i <= 12; i++)
+                    {
+                        Console.WriteLine($"{i} * {value} = {value * i}");
+                    }
+
+                    // set the boolean value to false to terminate the while loop
+                    isValid = false;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Number Entered.");
+                }
+            }
+
+
+
+            /*
+             * TASK
+             * 
+             */
             Console.ReadKey();
         }
     }
