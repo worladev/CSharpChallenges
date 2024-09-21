b737f06c9e300470b394d5fc9f1744692b164080
worladev <confidenceworlasi@gmail.com>
2024-09-20 22:28:27 -0400

    added a learning and task workspace project

---
+++ Colab/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WORKSPACE-1");
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+            //int userNum = Convert.ToInt32(Console.ReadLine());
+            //int userNum2 = Convert.ToInt32(Console.ReadLine());            
+            //int addition = add(userNum, userNum2);
+            //Console.WriteLine(addition);
+            Console.ReadKey();
+        }
+
+        int add(int num1, int num2)
+        {
+            int result;
+            result = num1 + num2;
+            return result;
+        }
+    }
+}
